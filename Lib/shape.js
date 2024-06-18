b2822672e2454c939df0ab4f48e521728da68b86
thomassmith0526 <thomassmith0526@gmail.com>
2024-06-18 13:10:24 -0500

    working on the test

--- Lib/shape.js
+++ Lib/shape.js
@@ -1,11 +1,49 @@
-function createShape(type, color){
-    return {
-        type: type,
-        color: color,
-        draw: function(){
-            return `Drawing a ${this.color} ${this.type} shape`;
-        }
-    }
-}
+
 const myShape = createShape('circle', 'red');
 console.log(myShape.draw)
+
+
+class Shape {
+    constructor(color, text, textColor) {
+        this.color = color;
+        this.text = text;
+        this.textColor = textColor;
+    }
+    printMetaData() {
+        console.log(`the color is ${this.color} it says ${this.text} the text color is ${this.textColor}`)
+    }
+}
+class Circle extends Shape {
+    constructor(color, text, textColor, type){
+        super(color, text, textColor);
+        this.type = type;
+    }
+    printMetaData() {
+        console.log(`the color is ${this.color} it says ${this.text} the text color is ${this.textColor} the shape is ${this.type}`)
+    }
+}
+class Triangle extends Shape {
+    constructor(color, text, textColor, type){
+        super(color, text, textColor);
+        this.type = type;
+    } 
+    printMetaData() {
+        console.log(`the color is ${this.color} it says ${this.text} the text color is ${this.textColor} the shape is ${this.type}`)
+    }
+}
+class Square extends Shape {
+    constructor(color, text, textColor, type){
+        super(color, text, textColor);
+        this.type = type;
+    }
+    printMetaData() {
+        console.log(`the color is ${this.color} it says ${this.text} the text color is ${this.textColor} the shape is ${this.type}`)
+    }
+
+    
+}
+
+const circleOne = new Circle('green', 'SVG','white', 'circle')
+
+
+circleOne.printMetaData()
--- index.js
+++ index.js
@@ -1,38 +1,81 @@
-const inquirer = require("inquirer")
-const fs = require("fs")
-const svg = require("svg")
+const inquirer = require('inquirer');
+const fs = require('fs')
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type : "input",
-            message: "What is the name of the company?",
-            name: "company",
-        }
-    ])
+// Function to generate SVG code based on user input
+function generateSVG(shapeType, text, shapeColor, textColor) {
+    // Construct SVG code based on user input
+    let svgCodeShape;
+
+    switch (shapeType) {
+        case 'circle':
+            svgCodeShape = `
+                <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+                    <circle r="95" cx="100" cy="100" fill="${shapeColor}" />
+                    <text x="100" y="100" fill="${textColor}" text-anchor="middle">${text}</text>
+                </svg>
+            `;
+            break;
+        case 'square':
+            svgCodeShape = `
+                <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="200" height="200" x="50" y="50" rx="0" ry="0" fill="${shapeColor}" />
+                    <text x="150" y="150" fill="${textColor}" text-anchor="middle">${text}</text>
+                </svg>
+            `;
+            break;
+        case 'triangle':
+            svgCodeShape = `
+                <svg height="220" width="500" xmlns="http://www.w3.org/2000/svg">
+                    <polygon points="100,10 150,190 50,190" style="fill:${shapeColor};stroke:${shapeColor};stroke-width:3" />
+                    <text x="100" y="100" fill="${textColor}" text-anchor="middle">${text}</text>
+                </svg>
+            `;
+            break;
+        default:
+            svgCodeShape = '';
+            break;
+    }
+
+    return svgCodeShape;
 }
 
+// Inquirer prompt to collect user input
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Enter the shape',
+        choices: [
+            "circle",
+            "triangle",
+            "square",
+        ]
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter the text to display: (no more than 3 letters)'
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color:'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color:'
+    }
+]).then(answers => {
+    // Generate SVG code based on user input
+    const svgCode = generateSVG(answers.shape, answers.text, answers.shapeColor, answers.textColor);
 
-class Shape {
-    constructor(color) {
-        this.color = color;
-    }
-}
-class Circle extends Shape {
-    constructor(color, type){
-        super(color);
-        this.type = type;
-    }
-}
-class Triangle extends Shape {
-    constructor(color, type) {
-        super(color);
-        this.type = type;
-    } 
-}
-class Square extends Shape {
-    constructor(color, type) {
-        super(color);
-        this.type = type;
-    }
-}
+    // Write SVG code to a file
+    fs.writeFileSync('output.svg', svgCode);
+    console.log('SVG file generated successfully!');
+});
+module.exports = generateSVG
+
+
+
+
